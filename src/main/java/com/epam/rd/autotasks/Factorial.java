a223d5bf8e58eeb901df324a9f65f7c0f374791f
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-10-27 21:50:04 +0300

    test ver1 - impl 3 test classes

--- src/main/java/com/epam/rd/autotasks/Factorial.java
+++ src/main/java/com/epam/rd/autotasks/Factorial.java
@@ -1,8 +1,39 @@
 package com.epam.rd.autotasks;
 
+import java.math.BigDecimal;
+
 public class Factorial {
+    //Design and code a factorial method in the Factorial class. Here are some details:
+    //the method takes a String as a parameter, transforms it to an integer value and counts its factorial.
+    //The method returns a result as a String.
+    //String parameter n must represent a non-negative integer number. If it does not, throw an IllegalArgumentException.
     public String factorial(String n) {
-        throw new UnsupportedOperationException();
+        if (n == null) {
+            throw new IllegalArgumentException();
+        }
+        if (n.matches("^[0-9]+\\.[0-9]+$")){
+            throw new IllegalArgumentException();
+        }
+        if (n.equals("")){
+            throw new IllegalArgumentException();
+        }
+        if (n.matches("[^0-9]")){
+            throw new IllegalArgumentException();
+        }
+        if (Integer.parseInt(n) < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        BigDecimal inputNum = new BigDecimal(n);
+        BigDecimal factorialResult = new BigDecimal(String.valueOf(1));
+        for (int i = 1; i <= inputNum.longValue(); i++) {
+            factorialResult = factorialResult.multiply(new BigDecimal(String.valueOf(i)));
+        }
+        return String.valueOf(factorialResult);
 
     }
+
+    /*public static void main(String[] args) {
+        System.out.println(factorial("5"));
+    }*/
 }
--- src/main/java/com/epam/rd/autotasks/FactorialBadInputTesting.java
+++ src/main/java/com/epam/rd/autotasks/FactorialBadInputTesting.java
@@ -1,30 +1,45 @@
 package com.epam.rd.autotasks;
 
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
 
 class FactorialBadInputTesting {
 
     Factorial factorial = new Factorial();
+    private final double EPS = 1e-9;
+
+
 
     @Test
-    void testNullInput(){
+    void testNullInput() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial("null"));
 
     }
 
     @Test
-    void testNegativeInput(){
+    void testNegativeInput() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial("-8"));
+    }
+
+
+
+    @Test
+    void testFractionalInput() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial("2.5"));
+
 
     }
 
     @Test
-    void testFractionalInput(){
-
+    void testNonDigitalInput() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial("zz"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.factorial(""));
     }
 
-    @Test
-    void testNonDigitalInput(){
 
-    }
 
 
 }
